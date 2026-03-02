a3af39eb41c0f9b06055f53f49147a1376eae0f9
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-02 10:25:42 +0100

    Move test bed initalization into test setup

---
+++ projects/ngx-emfular-integration/src/test-setup.ts
@@ -0,0 +1,11 @@
+import 'zone.js/testing';
+import { TestBed } from '@angular/core/testing';
+import {
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+TestBed.initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting(),
+);
