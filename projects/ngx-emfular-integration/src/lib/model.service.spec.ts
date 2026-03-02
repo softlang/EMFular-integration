b7890850b173d05776f2d7b3a2dc72e6a810318a
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-02 10:37:48 +0100

    First integration test creating a small service

--- projects/ngx-emfular-integration/src/lib/model.service.spec.ts
+++ projects/ngx-emfular-integration/src/lib/model.service.spec.ts
@@ -1,13 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ModelService } from './model.service';
+import {HISTORY_SERVICE, ModelService, provideHistoryForModel} from './model.service';
+import {HistoryService, IoService } from 'ngx-emfular-helper';
+import { Referencable } from 'emfular';
 
 describe('ModelServiceService', () => {
+
+  class DummyModel extends Referencable<any>{
+    id = 'x';
+    constructor() {
+      super();
+    }
+  }
+
+  class DummyModelService extends ModelService<DummyModel> {
+    constructor(
+        history: HistoryService<DummyModel>,
+        io: IoService
+    ) {
+      super(history, io, DummyModel);
+    }
+  }
+
   let service: ModelService<any>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ModelService);
+    TestBed.configureTestingModule({
+      providers: [
+          provideHistoryForModel('test_', 10),
+        { provide: IoService, useClass: IoService },
+        { provide: DummyModelService, useFactory: () =>
+              new DummyModelService(
+                  TestBed.inject(HISTORY_SERVICE),
+                  TestBed.inject(IoService),
+              ) }
+      ]
+    });
+    service = TestBed.inject(DummyModelService);
   });
 
   it('should be created', () => {
