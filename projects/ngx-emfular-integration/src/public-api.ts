99fd006a12c08356f310514331438fa5b4e41364
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-05 21:09:56 +0100

    Remove now unused childcomponent

--- projects/ngx-emfular-integration/src/public-api.ts
+++ projects/ngx-emfular-integration/src/public-api.ts
@@ -8,4 +8,3 @@ export * from './lib/editor/file-level-bar/file-level-bar.component';
 export * from './lib/editor/file-level-bar-material/file-level-bar-material.component';
 export * from './lib/editor/model-editing-bar/model-editing-bar.component';
 export * from './lib/referencable-box/referencable-box.component';
-export * from './lib/referencable-child-box/referencable-child-box.component';
