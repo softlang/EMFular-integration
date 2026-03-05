109e91874060fe07a867d360bb935c4680757ed3
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-05 12:24:13 +0100

    Simplify children export into list all the time, also refactor some graphical stuf

--- projects/ngx-emfular-integration/src/lib/referencable-box/referencable-box.component.ts
+++ projects/ngx-emfular-integration/src/lib/referencable-box/referencable-box.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {Referencable} from 'emfular';
+import {ReContainer, Referencable} from 'emfular';
 import {BoundingBox, RectangleComponent, TextAreaSvgComponent} from 'ngx-svg-graphics';
 import {NgForOf} from "@angular/common";
 import {ReferencableChildBoxComponent} from "../referencable-child-box/referencable-child-box.component";
@@ -17,16 +17,31 @@ export class ReferencableBoxComponent {
 
   constructor() {}
 
+  //todo static, refactor into emfular
+  getAsList<T extends Referencable<any>>(refs: ReContainer<T, any>): T[] {
+    const items: T[]|T|undefined = refs.get()
+    if(items) {
+      if(Array.isArray(items)) {
+        return items
+      }
+      const result: T[] = [];
+      result.push(items)
+      return result
+    } else {
+      return [];
+    }
+  }
+
   static computeOffset(index: number, length: number): number {
     const middle = (length-1)/2;
     return index - middle;
   }
   computeChildBBox(index: number, length: number): BoundingBox {
     return {
-      x: this.position.x + ReferencableBoxComponent.computeOffset(index, length)*55,
+      x: this.position.x + ReferencableBoxComponent.computeOffset(index, length)*(this.position.w+5),
       y: this.position.y+80,
-      w: 50,
-      h: 20
+      w: 400,
+      h: 25
     }
   }
 }
--- projects/ngx-emfular-integration/src/lib/referencable-child-box/referencable-child-box.component.ts
+++ projects/ngx-emfular-integration/src/lib/referencable-child-box/referencable-child-box.component.ts
@@ -1,6 +1,11 @@
 import {Component, Input} from '@angular/core';
 import {Referencable} from "emfular";
-import { BoundingBox, RectangleComponent, TextAreaSvgComponent } from 'ngx-svg-graphics';
+import {
+  ArrowBetweenElemsComponent,
+  BoundingBox,
+  RectangleComponent,
+  TextAreaSvgComponent
+} from 'ngx-svg-graphics';
 import {NgForOf} from "@angular/common";
 
 @Component({
@@ -8,16 +13,15 @@ import {NgForOf} from "@angular/common";
   imports: [
     NgForOf,
     RectangleComponent,
-    TextAreaSvgComponent
+    TextAreaSvgComponent,
+    ArrowBetweenElemsComponent,
   ],
   templateUrl: './referencable-child-box.component.svg',
   styleUrl: './referencable-child-box.component.css'
 })
 export class ReferencableChildBoxComponent {
   @Input() refName!: string;
-  @Input() isSingle = false;
-  @Input() children?: Referencable<any>[]
-  @Input() child?: Referencable<any>
+  @Input() children: Referencable<any>[] = []
   @Input() parentGId!: string;
   @Input() middle!: BoundingBox
 
