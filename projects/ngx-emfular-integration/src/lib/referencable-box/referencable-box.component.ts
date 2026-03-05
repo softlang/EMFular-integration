cc778d48c4d611cb933503d1856a9a8e1295cddb
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-05 21:04:54 +0100

    Making referencable box self-recursive

--- projects/ngx-emfular-integration/src/lib/referencable-box/referencable-box.component.ts
+++ projects/ngx-emfular-integration/src/lib/referencable-box/referencable-box.component.ts
@@ -1,13 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {Referencable} from 'emfular';
-import {BoundingBox, RectangleComponent, TextAreaSvgComponent} from 'ngx-svg-graphics';
+import {ArrowBetweenElemsComponent, BoundingBox, RectangleComponent, TextAreaSvgComponent} from 'ngx-svg-graphics';
 import {NgForOf} from "@angular/common";
-import {ReferencableChildBoxComponent} from "../referencable-child-box/referencable-child-box.component";
 import {GraphicalHelper} from "../graphical/graphical-helper";
 
 @Component({
   selector: '[referencable-box]',
-  imports: [RectangleComponent, TextAreaSvgComponent, NgForOf, ReferencableChildBoxComponent, TextAreaSvgComponent],
+  imports: [RectangleComponent, TextAreaSvgComponent, NgForOf, TextAreaSvgComponent, ArrowBetweenElemsComponent],
   templateUrl: './referencable-box.component.svg',
   styleUrl: './referencable-box.component.css'
 })
