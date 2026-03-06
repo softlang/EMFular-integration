809dc1e3c5567488a4f5dbad8a60f80318dc7deb
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-06 16:24:18 +0100

    First editing facility for attributes (no references yet) - all attributes are treated as strings

--- projects/ngx-emfular-integration/src/lib/details/model-details/model-details.component.ts
+++ projects/ngx-emfular-integration/src/lib/details/model-details/model-details.component.ts
@@ -1,16 +1,30 @@
-import {Component, Input} from '@angular/core';
-import { Referencable } from 'emfular';
+import {Component, Input, OnInit} from '@angular/core';
+import {AttributeOptions, Referencable } from 'emfular';
 import {ModelService} from "../../model.service";
+import { getAllAttributes } from "emfular";
+import {FormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'lib-model-details',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './model-details.component.html',
   styleUrl: './model-details.component.css'
 })
-export class ModelDetailsComponent<T extends Referencable<any>, M extends Referencable<any>> {
-  @Input() model!:T
+export class ModelDetailsComponent<T extends Referencable<any>, M extends Referencable<any>> implements OnInit {
+  @Input() model!: T
   @Input() modelService!: ModelService<M>
 
-}
+  attributes: Array<{ key: string; options: AttributeOptions }> = [];
 
+  ngOnInit() {
+    const map = getAllAttributes(this.model.constructor);
+    this.attributes = Array.from(map.entries()).map(([key, options]) => ({
+      key,
+      options
+    }));
+  }
+}
