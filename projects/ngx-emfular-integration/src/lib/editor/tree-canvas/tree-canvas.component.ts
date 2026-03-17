43d7b3a45b08f752546c296d1f928c6901da0f2b
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-17 17:35:01 +0100

    Use different tree canvas, just for details service

--- projects/ngx-emfular-integration/src/lib/editor/tree-canvas/tree-canvas.component.ts
+++ projects/ngx-emfular-integration/src/lib/editor/tree-canvas/tree-canvas.component.ts
@@ -1,18 +1,20 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {ReferencableBoxComponent} from "../../graphical/referencable-box/referencable-box.component";
 import {ModelService} from "../../model.service";
 import { Referencable, ReTreeChildrenContainer } from 'emfular';
 import { BoundingBox } from 'ngx-svg-graphics';
+import {ModelCanvasComponent} from "../model-canvas/model-canvas.component";
 
 @Component({
   selector: 'tree-canvas',
-    imports: [
-        ReferencableBoxComponent
-    ],
+  imports: [
+    ReferencableBoxComponent,
+    ModelCanvasComponent
+  ],
   templateUrl: './tree-canvas.component.html',
   styleUrl: './tree-canvas.component.css'
 })
-export class TreeCanvasComponent<M extends Referencable<any>> implements OnInit {
+export class TreeCanvasComponent<M extends Referencable<any>> {
   svgwidth = 1500;
   svgheigth = 1000;
   initialBBox : BoundingBox = {x: this.svgwidth/2, y: 20, w: 200, h: 25}
@@ -21,13 +23,6 @@ export class TreeCanvasComponent<M extends Referencable<any>> implements OnInit 
   @Output() chooseElement: EventEmitter<Referencable<any>> = new EventEmitter();
   @Output() chooseReference: EventEmitter<ReTreeChildrenContainer<any>> = new EventEmitter();
   @Output() svgReady: EventEmitter<SVGSVGElement> = new EventEmitter<SVGSVGElement>();
-  @ViewChild('svg', { static: true })
-  svg!: ElementRef<SVGSVGElement>;
-
-  ngOnInit() {
-    // Safe because static: true
-    this.svgReady.emit(this.svg.nativeElement);
-  }
 
   choose(element: Referencable<any>): void {
     this.chooseElement.emit(element);
