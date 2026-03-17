b0a7790478590983963aa2829368d22b4db409b2
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-17 19:24:46 +0100

    Export new componenzs

--- projects/ngx-emfular-integration/src/public-api.ts
+++ projects/ngx-emfular-integration/src/public-api.ts
@@ -9,6 +9,8 @@ export * from './lib/editor/file-level-bar/file-level-bar.component';
 export * from './lib/editor/file-level-bar-material/file-level-bar-material.component';
 export * from './lib/editor/model-editing-bar/model-editing-bar.component';
 export * from './lib/editor/tree-canvas/tree-canvas.component';
+export * from './lib/editor/model-canvas/model-canvas.component';
+export * from './lib/editor/basic-editor/basic-editor.component';
 export * from './lib/editor/edit-button-def';
 
 export * from './lib/graphical/referencable-box/referencable-box.component';
