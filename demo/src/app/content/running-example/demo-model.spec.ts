ff6a3afee0febb337fe0c5ce83b7cacd08016b34
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-18 11:35:50 +0200

    Remove wrongly added angular cache files and avoid further addition via gitignore, also add a small running example

---
+++ demo/src/app/content/running-example/demo-model.spec.ts
@@ -0,0 +1,7 @@
+import { DemoElement1 } from './demo-model';
+
+describe('DemoElement1', () => {
+  it('should create an instance', () => {
+    expect(new DemoElement1()).toBeTruthy();
+  });
+});
---
+++ demo/src/app/content/running-example/demo-model.ts
@@ -0,0 +1,36 @@
+import {eClass, ModelDefinition, ModelList, Referencable, reference, ReferenceMeta} from "emfular-core";
+
+export const elem1Refs = {
+    children: {
+        target: "DemoElement2",
+        containment: true,
+        min: 0,
+        max: -1,
+    } satisfies ReferenceMeta
+}
+
+export const DemoElementMeta: ModelDefinition = {
+    name: "demo",
+    prefix: "demo",
+    uri: "demo//",
+    classes: {
+        DemoElement1: {
+            references: {}
+        }
+    }
+}
+
+@eClass(DemoElementMeta, "DemoElement1")
+export class DemoElement1 extends Referencable<any>{
+
+    constructor() {
+        super();
+    }
+
+    @reference(elem1Refs.children)
+    declare children: ModelList<DemoElement2>
+}
+
+@eClass(DemoElementMeta, "DemoElement1")
+export class DemoElement2 extends Referencable<any>{
+}
