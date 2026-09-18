76aeb1707aa1e6f2f3ee710c7cf328da7c0ee17f
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-18 12:18:56 +0200

    Editor component

---
+++ demo/src/app/content/editor/editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-editor',
+  imports: [],
+  templateUrl: './editor.component.html',
+  styleUrl: './editor.component.css'
+})
+export class EditorComponent {
+
+}
