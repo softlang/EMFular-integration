fb4a42be546e4b7232c89b3b68560eb3cb726716
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-20 09:02:28 +0200

    Explain Buttondef and use it on the model editing bar. It now has disabled available, and uses it

--- demo/src/app/content/editor/editor.component.ts
+++ demo/src/app/content/editor/editor.component.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
 import {HighlightedCodeComponent} from "../../layout/highlighted-code/highlighted-code.component";
 import {editButtonDef} from "./editor.component.code";
+import {
+  EditButtonDef,
+  ModelEditingBarComponent
+} from "ngx-emfular-integration";
 
 @Component({
   selector: 'demo-editor',
   imports: [
-    HighlightedCodeComponent
+    HighlightedCodeComponent,
+    ModelEditingBarComponent
   ],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.css'
 })
 export class EditorComponent {
+
+  buttons0: EditButtonDef[] = [
+    {label: "label1", action: () =>this.buttonAction("label1")},
+    {label: "label2", action: () =>this.buttonAction("label2")},
+    {label: "reset", action: () =>this.buttonReset(), disabled: true},
+  ]
+  private adaptResetButton(value: boolean) {
+    this.buttons0[2].disabled = value
+  }
+
+  initialMsg0 = 'click the buttons: '
+  buttonMessage0: string = this.initialMsg0
+
+  buttonAction(str: string) {
+    this.buttonMessage0+=str+" clicked "
+    this.adaptResetButton(false)
+  }
+
+  buttonReset() {
+    this.buttonMessage0 = this.initialMsg0;
+    this.adaptResetButton(true);
+  }
 
   protected readonly editButtonDef = editButtonDef;
 }
