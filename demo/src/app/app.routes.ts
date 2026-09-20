b652996f20b9b8368cb93ed61d2e3fe6f0402680
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-20 20:19:59 +0200

    Started details component

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./content/home/home.component";
 import {EditorComponent} from "./content/editor/editor.component";
+import {DetailsComponent} from "./content/details/details.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'details', component: DetailsComponent },
     { path: 'editor', component: EditorComponent },
 ];
