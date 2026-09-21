6eb984d1788645c1c1a51c396e328b9a9a4212ba
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-21 13:22:33 +0200

    Graphical component shortly explained and visualized

--- demo/src/app/content/graphical/graphical.component.ts
+++ demo/src/app/content/graphical/graphical.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import {BoundingBox, SvgCanvasComponent} from "ngx-emfular-diagram";
+import {
+  ReferencableBoxComponent
+} from "ngx-emfular-integration";
+import {DemoElement1, DemoElement2} from "../running-example/demo-model";
+import {DemoModelService} from "../running-example/demo-model.service";
 
 @Component({
   selector: 'demo-graphical',
-  imports: [],
+  imports: [
+    SvgCanvasComponent,
+    ReferencableBoxComponent
+  ],
   templateUrl: './graphical.component.html',
   styleUrl: './graphical.component.css'
 })
 export class GraphicalComponent {
 
+  model0: DemoElement1;
+  model01: DemoElement2;
+  model02: DemoElement2;
+
+  initialBbox: BoundingBox = {x: 500, y: 0, w: 200, h: 50}
+
+
+  constructor(public readonly demoModelService: DemoModelService) {
+    this.model0 = new DemoElement1('model0')
+    this.model01 = new DemoElement2('model01')
+    this.model02 = new DemoElement2('model02')
+    this.model0.children.push(this.model01, this.model02)
+    this.model01.friends.push(this.model02)
+  }
 }
