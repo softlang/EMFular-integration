71141ef4b8fbdbda5904be4ff96ef3b861505f1c
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-25 12:05:06 +0200

    use diagram canvas on basic editor

--- src/lib/editor/editors/basic-editor/basic-editor.component.ts
+++ src/lib/editor/editors/basic-editor/basic-editor.component.ts
@@ -1,24 +1,34 @@
 import { Referencable } from "emfular-core";
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnChanges, SimpleChanges} from "@angular/core";
 import {ModelService} from "../../../model.service";
-import {ModelCanvasComponent} from "../../canvases/model-canvas/model-canvas.component";
+import {SvgCanvasComponent} from "ngx-emfular-diagram";
 import {FileLevelBarComponent} from "../../editor-bars/file-level-bar/file-level-bar.component";
 
 @Component({
   selector: 'emfular-basic-editor',
   standalone: true,
-  imports: [ModelCanvasComponent, FileLevelBarComponent],
+  imports: [SvgCanvasComponent, FileLevelBarComponent],
   templateUrl: './basic-editor.component.html',
   styleUrls: ['./basic-editor.component.css']
 })
-export class BasicEditorComponent<M extends Referencable<any>> {
+export class BasicEditorComponent<
+    M extends Referencable<any>
+> implements OnChanges {
   @Input() modelService!: ModelService<M>;
   @Input() svgwidth = 1500;
   @Input() svgheight = 1000;
 
   svgElement!: SVGSVGElement;
 
+  viewBox: string = "0 0 1500 1000"
+
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.svgwidth || changes.svgheight) {
+      this.viewBox = "0 0 "+this.svgwidth+" "+this.svgheight;
+    }
+  }
 
   onSvgReady(svg: SVGSVGElement) {
     queueMicrotask(() => this.svgElement = svg);
