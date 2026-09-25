bad3b30835ac41b0b99d54df6becd0d937fb288f
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-25 11:40:28 +0200

    Add demos of complete editors to the editor demo page

--- demo/src/app/content/editor/editor.component.ts
+++ demo/src/app/content/editor/editor.component.ts
@@ -2,25 +2,40 @@ import { Component } from '@angular/core';
 import {HighlightedCodeComponent} from "../../layout/highlighted-code/highlighted-code.component";
 import {editButtonDef} from "./editor.component.code";
 import {
-  EditButtonDef, FileLevelBarComponent,
-  ModelEditingBarComponent
+    BasicEditorComponent,
+    EditButtonDef,
+    FileLevelBarComponent,
+    ModelEditingBarComponent,
+    ReferencableBoxComponent,
+    TreeEditorComponent
 } from "ngx-emfular-integration";
 import {DemoModelService} from "../running-example/demo-model.service";
+import {DemoElement2} from "../running-example/demo-model";
 
 @Component({
   selector: 'demo-editor',
-  imports: [
-    HighlightedCodeComponent,
-    ModelEditingBarComponent,
-    FileLevelBarComponent
-  ],
+    imports: [
+        HighlightedCodeComponent,
+        ModelEditingBarComponent,
+        FileLevelBarComponent,
+        BasicEditorComponent,
+        ReferencableBoxComponent,
+        ReferencableBoxComponent,
+        TreeEditorComponent
+    ],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.css'
 })
 export class EditorComponent {
 
   constructor(public readonly demoModelService: DemoModelService) {
+      this.demoModelService.model.name = 'model0'
+      const model01 = new DemoElement2('model01')
+      const model02 = new DemoElement2('model02')
+      this.demoModelService.model.children.push(model01, model02)
+      model01.friends.push(model02)
   }
+
 
   buttons0: EditButtonDef[] = [
     {label: "label1", action: () =>this.buttonAction("label1")},
@@ -43,6 +58,9 @@ export class EditorComponent {
     this.buttonMessage0 = this.initialMsg0;
     this.adaptResetButton(true);
   }
+
+  positionCompleteFirstElem = {x: 300, y: 0, w:80, h: 20};
+
 
   protected readonly editButtonDef = editButtonDef;
 }
